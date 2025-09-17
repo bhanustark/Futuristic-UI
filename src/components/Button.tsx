6dbf77d418a1fad3f1e3323bd7ef733346938968
bhanustark <bhanustark@rediffmail.com>
2025-09-17 18:00:33 +0530

    first commit

---
+++ src/components/Button.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Button = ({
+  children,
+  onClick,
+}: {
+  children: React.ReactNode;
+  onClick: () => void;
+}) => {
+  return <button onClick={onClick}>{children}</button>;
+};
+
+export default Button;
---
+++ vite.config.ts
@@ -0,0 +1,24 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  build: {
+    lib: {
+      entry: "src/index.js",
+      name: "FutureUI",
+      fileName: (format) => `future-ui.${format}.js`,
+    },
+    rollupOptions: {
+      // Ensure to externalize dependencies that shouldn't be bundled
+      external: ["react", "react-dom"],
+      output: {
+        globals: {
+          react: "React",
+          "react-dom": "ReactDOM",
+        },
+      },
+    },
+  },
+});
