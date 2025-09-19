74b5099a6204f9623100eec466ed67df02c470ab
bhanustark <bhanustark@rediffmail.com>
2025-09-19 15:12:58 +0530

    removed port

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,6 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   server: {
-    port: 5578,
     allowedHosts: ["localhost", "futuristicui.com", "www.futuristicui.com"],
   },
   plugins: [react(), tailwindcss()],
